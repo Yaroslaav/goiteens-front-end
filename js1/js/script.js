f919854591db119acb692c6f13c1e79de736c6fe
Yaroslaav <cahloyaroslav@gmail.com>
2021-01-29 16:30:16 +0200

    hw_js1

---
+++ js1/js/script.js
@@ -0,0 +1,15 @@
+// String
+const d = 'Привіт!';
+console.log(typeof d); // "string" 
+
+// Number
+const c = 123;
+console.log(typeof c); // "number"
+
+// flag
+const flag = true;
+console.log(typeof flag); // "flag"
+
+// txt
+const txt = 'true';
+console.log(typeof null); // "txt"
