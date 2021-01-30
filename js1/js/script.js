d2fb6391594d67a71e6b89edba2cc3a08ca78141
Yaroslaav <cahloyaroslav@gmail.com>
2021-01-30 09:49:07 +0200

    hw js 1

--- js1/js/script.js
+++ js1/js/script.js
@@ -1,15 +1,40 @@
-// String
-const d = 'Привіт!';
-console.log(typeof d); // "string" 
+'use strich'
 
-// Number
-const c = 123;
-console.log(typeof c); // "number"
+// str
+const str = 'Привіт';
+console.log('Змінна str належить до типу', typeof str); //string
+
+// num
+const num = 123;
+console.log('Змінна num належить до типу', typeof num); //number
 
 // flag
 const flag = true;
-console.log(typeof flag); // "flag"
+console.log('Змінна flag належить до типу', typeof flag); //bolean
 
 // txt
 const txt = 'true';
-console.log(typeof null); // "txt"
+console.log('Змінна txt належить до типу', typeof txt); //string
+
+// Ім'я
+const name ='Ярик';
+console.log("Ім'я учня", name);
+
+// Вік
+let eyars = 12;
+console.log(('Його вік'), eyars);
+
+// Клас в я кому вчиться Ярик
+
+let clas = 7;
+console.log(('Він вчиться в'), clas, ('класі'))
+
+// Кількість справжніх друзів
+let friends = 4;
+console.log('У Ярика є', friends, 'справжніх друга')
+
+let bash = 4;
+console.log('Ярик настроював git bash', bash, 'рази')
+
+let kaktus = 23;
+console.log('У Ярика є', kaktus, 'кактуси')
