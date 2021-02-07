0ea201d07685ed61cdf083367310677e3f9f3a54
Yaroslaav <cahloyaroslav@gmail.com>
2021-02-07 15:58:51 +0200

    js3

--- js3/js/script.js
+++ js3/js/script.js
@@ -1,15 +1,15 @@
 'use strich'
 // завдання 1
 
-// const k1 = 5px;
-// const k2 = 12djd334;
-// const k3 = 12.45asdwe12;
-// const k4 = qwqweeewq;
+const k1 = '5px';
+const k2 = '12djd334';
+const k3 = '12.45asdwe12';
+const k4 = 'qwqweeewq';
 
-// console.log(Math.round(k1));
-// console.log(Math.round(k2));
-// console.log(Math.round(k3));
-// console.log(Math.round(k4));
+console.log(Math.round(k1));
+console.log(Math.round(k2));
+console.log(Math.round(k3));
+console.log(Math.round(k4));
 
 
 // завдання 2
@@ -34,13 +34,41 @@ console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18))
 // завдання 4
 
 // варіант 1
-let random = Math.round(Math.random() * (19 - 1) + 1);
+let random = Math.round(Math.random() * (19 - 3) + 3);
 console.log(random); 
 
 // варіант 2
 
-console.log(Math.round(Math.random() * (19 - 1) + 1));
+console.log(Math.round(Math.random() * (19 - 3) + 3));
 
 
+//  завдання 5
+
+let  result= 5+5+'5';
+console.log("5+5+'5'=", result); 
+console.log("typeof 5+5+'5':", typeof result);
 
 
+// завдання 6
+
+let email = 'cahloyaroslav@gmail.com';   
+console.log('Чи є в змінній email такий символ як @?' ,  'Відповідь:', email.includes('@'));
+console.log('В змінній email', email.length, 'символи');
+
+
+// завдання 7
+
+let My = 'My ';
+let nam = 'name ';
+let is = 'is ';
+let fullName = My + nam + is + 'Victor';
+console.log(fullName)
+
+
+// завдання 8
+
+const userName = 'yarik';
+let payment = 300;
+console.log(alert('Дякуємо, ' + userName +'! До сплати ' + payment  + ' гривень'));
+
+console.log('Дякуємо, ' + userName +'! До сплати ' + payment  + ' гривень');
