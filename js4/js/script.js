1f79983784a7a7510f7d302ef3073888d75c7f04
Yaroslaav <cahloyaroslav@gmail.com>
2021-02-18 18:46:13 +0200

    js4-5\

--- js4/js/script.js
+++ js4/js/script.js
@@ -6,7 +6,7 @@
 let number = Number(prompt("Введіть число: "));
 console.log(number);
 
-if(number>55 && number<99){
+if(number>=55 && number<=99){
    console.log('Число потрапляє в діапазон');
 }else{
    console.log('Число не потрапляє в діапазон');
@@ -17,8 +17,6 @@ if(number>55 && number<99){
 
 // let age = Number(prompt("Введіть вік: "));
 let age = 55;
-
-if(age>=0&&age<=16 || age>=17&&age<=60 || age>=61&&age<=100){
     
    if (age>=0 && age<=16){
        console.log('Макс в групі 1');
@@ -31,8 +29,6 @@ if(age>=0&&age<=16 || age>=17&&age<=60 || age>=61&&age<=100){
    else if (age>=61 && age<=100){
        console.log('Макс в групі 3');
    }
-
-}
 
 
 // Завдання 3
@@ -54,29 +50,11 @@ if(userName.length>=4 && userSurname.length>=5){
 let random = Math.round(Math.random() * (5 - 1) + 1);
 console.log(random);
 
-if((random==1) || (random==2) || (random==3) || (random==4) || (random==5)){
-
-   if(random==1){
-      console.log("random = 1");
-   }
-
-   else if(random==2){
-      console.log("random = 2");
-   }
-
-   else if(random==3){
-      console.log("random = 3");
-   }
-   
-   else if(random==4){
-      console.log("random = 4");
-   }
-   
-   else if(random==5){
-      console.log("random = 5");
-   }
+if(random>=1&&random<=5){
+   console.log(random);
+}else{
+   console.log('число не потрапляє на проміжок від 1 - 5');
 }
-
 
 //Завдання 5
 
@@ -106,5 +84,3 @@ switch (lang) {
   default:
     console.log('Invalid lang type');
 }
-
-// console.log(lang); // 500
---
+++ js5/js/script.js
@@ -0,0 +1,40 @@
+'use strich'
+
+
+// Завдання 1
+
+let counter = 1;
+
+while (counter < 10) {
+  console.log('counter: ', counter);
+  counter++;
+}
+
+
+// Завдання 2
+
+
+
+
+
+// Завдання 3
+
+let num = 20;
+
+while (num <= 28) {
+   console.log('num: ', num);
+   num++;
+ }
+
+
+//  Завдання 4
+
+let name = '';
+let lenghter = Number(name.length); 
+
+
+while (lenghter<= 6){
+   name = prompt("Введіть ім'я улюбленого героя");
+   console.log(name);
+   break
+ }
